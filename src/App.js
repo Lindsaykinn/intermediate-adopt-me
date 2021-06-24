efc81f4aa5b7cee29faf2817df49d80e88b3be42
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-06-24 15:46:49 -0400

    added styling to header banner

--- src/App.js
+++ src/App.js
@@ -38,8 +38,8 @@ const App = () => {
         }}
       >
         <Router>
-          <header>
-            <Link to="/">
+          <header className="w-full m-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500">
+            <Link to="/" className="text-6xl text-white hover:text-gray-300">
               <h1>Adopt Me!</h1>
             </Link>
           </header>
