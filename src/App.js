c5113557f6456aadb03cbcaf1f883fdb776fd02e
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-06-24 16:49:44 -0400

    fixed banner centering

--- src/App.js
+++ src/App.js
@@ -32,13 +32,13 @@ const App = () => {
   return (
     <ThemeContext.Provider value={themeHook}>
       <div
-        className="p-0 mb-0"
+        className="p-0 m-0"
         style={{
           background: "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg",
         }}
       >
         <Router>
-          <header className="w-full m-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500">
+          <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500">
             <Link to="/" className="text-6xl text-white hover:text-gray-300">
               <h1>Adopt Me!</h1>
             </Link>
